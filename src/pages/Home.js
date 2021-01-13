1761740c82f94b2a51a16d02e36d80bcc487b386
YashKandalkar <yashsk.yk@gmail.com>
2021-01-13 11:57:54 +0530

    feat: add empty icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
-import {Typography} from 'antd';
+import {Typography, Empty} from 'antd';
 
 const {Title, Text} = Typography;
 
 const Home = () => {
     return (
-        <div style={{textAlign: "center"}}>
-            <Title level={3}>Creating forms is not yet implemented for public!</Title>
-            <Text>Ask the admin for a link of an ongoing exam!</Text>
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            textAlign: "center"
+        }}>
+            <div>
+                <Title level={3}>Creating forms is not yet implemented for public!</Title>
+                <Text>Ask the admin for a link of an open form!</Text>
+            </div>
+            <Empty description={""}/>
         </div>
     )
 }
